229bec7ba657e619179ffd8ab93332b517e7f50f
Faye <imprecisescience@gmail.com>
2022-05-21 18:14:00 +0800

    Fix useEffect warning

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react'
-import tw from 'tailwind-styled-components'
-import Highcharts from 'highcharts'
+import { useState, useEffect } from "react";
+import tw from "tailwind-styled-components";
+import Highcharts from "highcharts";
 import {
   HighchartsChart,
   HighchartsProvider,
@@ -9,14 +9,14 @@ import {
   Title,
   Series,
   Tooltip,
-} from 'react-jsx-highcharts'
+} from "react-jsx-highcharts";
 
 const Chart = ({ birthDate1, birthDate2, midDate }) => {
-  const [chartData, setChartData] = useState(null)
+  const [chartData, setChartData] = useState(null);
 
   const chartOptions = {
     height: 300,
-  }
+  };
 
   const plotOptions = {
     areaspline: {
@@ -33,168 +33,169 @@ const Chart = ({ birthDate1, birthDate2, midDate }) => {
       //pointStart: midDate - 86400000 * 60, // 60 days before today
       tooltip: {
         dateTimeLabelFormats: {
-          millisecond: '%b %e, %Y (%a)',
+          millisecond: "%b %e, %Y (%a)",
         },
       },
     },
-  }
+  };
 
   const generateTimeSeries = () => {
-    const oneDay = 1000 * 60 * 60 * 24
-    const startDate = midDate - oneDay * 60 // 60 days before today
-    const endDate = midDate + oneDay * 60 // 60 days after today
+    const oneDay = 1000 * 60 * 60 * 24;
+    const startDate = midDate - oneDay * 60; // 60 days before today
+    const endDate = midDate + oneDay * 60; // 60 days after today
 
-    let newPhysicalData1 = []
-    let newEmotionData1 = []
-    let newIqData1 = []
-    let newPhysicalData2 = []
-    let newEmotionData2 = []
-    let newIqData2 = []
-    let i = startDate
+    let newPhysicalData1 = [];
+    let newEmotionData1 = [];
+    let newIqData1 = [];
+    let newPhysicalData2 = [];
+    let newEmotionData2 = [];
+    let newIqData2 = [];
+    let i = startDate;
     while (i <= endDate) {
-      let daysFromBirth = (i - birthDate1) / oneDay
-      let physical = 10 * Math.sin(((2 * Math.PI) / 23) * daysFromBirth)
-      let emotion = 10 * Math.sin(((2 * Math.PI) / 28) * daysFromBirth)
-      let iq = 10 * Math.sin(((2 * Math.PI) / 33) * daysFromBirth)
-      newPhysicalData1.push([i, physical])
-      newEmotionData1.push([i, emotion])
-      newIqData1.push([i, iq])
+      let daysFromBirth = (i - birthDate1) / oneDay;
+      let physical = 10 * Math.sin(((2 * Math.PI) / 23) * daysFromBirth);
+      let emotion = 10 * Math.sin(((2 * Math.PI) / 28) * daysFromBirth);
+      let iq = 10 * Math.sin(((2 * Math.PI) / 33) * daysFromBirth);
+      newPhysicalData1.push([i, physical]);
+      newEmotionData1.push([i, emotion]);
+      newIqData1.push([i, iq]);
 
-      daysFromBirth = (i - birthDate2) / oneDay
-      physical = 10 * Math.sin(((2 * Math.PI) / 23) * daysFromBirth)
-      emotion = 10 * Math.sin(((2 * Math.PI) / 28) * daysFromBirth)
-      iq = 10 * Math.sin(((2 * Math.PI) / 33) * daysFromBirth)
-      newPhysicalData2.push([i, physical])
-      newEmotionData2.push([i, emotion])
-      newIqData2.push([i, iq])
+      daysFromBirth = (i - birthDate2) / oneDay;
+      physical = 10 * Math.sin(((2 * Math.PI) / 23) * daysFromBirth);
+      emotion = 10 * Math.sin(((2 * Math.PI) / 28) * daysFromBirth);
+      iq = 10 * Math.sin(((2 * Math.PI) / 33) * daysFromBirth);
+      newPhysicalData2.push([i, physical]);
+      newEmotionData2.push([i, emotion]);
+      newIqData2.push([i, iq]);
 
-      i += oneDay
+      i += oneDay;
     }
     setChartData([
       { physical: newPhysicalData1, emotion: newEmotionData1, iq: newIqData1 },
       { physical: newPhysicalData2, emotion: newEmotionData2, iq: newIqData2 },
-    ])
-  }
+    ]);
+  };
 
   useEffect(() => {
-    generateTimeSeries()
-  }, [birthDate1, birthDate2, midDate])
+    generateTimeSeries();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [birthDate1, birthDate2, midDate]);
 
   const handleMouseMove = (e) => {
-    let point = null
-    let event = null
+    let point = null;
+    let event = null;
 
-    e.persist()
+    e.persist();
     Highcharts.charts.forEach((chart) => {
-      if (!chart) return
-      event = chart.pointer.normalize(e) // Find coordinates within the chart
-      let points = []
+      if (!chart) return;
+      event = chart.pointer.normalize(e); // Find coordinates within the chart
+      let points = [];
       chart.series.forEach((s) => {
-        point = s.searchPoint(event, true) // Get the hovered point
+        point = s.searchPoint(event, true); // Get the hovered point
         if (point) {
-          points.push(point)
+          points.push(point);
         }
-      })
+      });
 
       if (points.length) {
         if (chart.tooltip.shared) {
-          chart.tooltip.refresh(points)
+          chart.tooltip.refresh(points);
         } else {
-          chart.tooltip.refresh(points[0])
+          chart.tooltip.refresh(points[0]);
         }
-        chart.xAxis[0].drawCrosshair(e, points[0])
+        chart.xAxis[0].drawCrosshair(e, points[0]);
       }
-    })
-  }
+    });
+  };
 
   const tooltipPositioner = function () {
-    return { x: this.chart.chartWidth - this.label.width - 20, y: 10 }
-  }
+    return { x: this.chart.chartWidth - this.label.width - 20, y: 10 };
+  };
 
   return (
     chartData && (
       <Wrapper>
         <div onMouseMove={handleMouseMove} onTouchMove={handleMouseMove}>
-          <HighchartsProvider Highcharts={Highcharts} key='Person1'>
+          <HighchartsProvider Highcharts={Highcharts} key="Person1">
             <HighchartsChart plotOptions={plotOptions} chart={chartOptions}>
-              <Title align='left' margin={30} x={30}>
-                {new Date(birthDate1).toLocaleDateString('en-us', {
-                  weekday: 'short',
-                  year: 'numeric',
-                  month: 'short',
-                  day: 'numeric',
+              <Title align="left" margin={30} x={30}>
+                {new Date(birthDate1).toLocaleDateString("en-us", {
+                  weekday: "short",
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
                 })}
               </Title>
 
-              <XAxis crosshair type='datetime' />
+              <XAxis crosshair type="datetime" />
               <YAxis>
                 <Series
-                  name='Physical'
-                  type='areaspline'
+                  name="Physical"
+                  type="areaspline"
                   data={chartData[0].physical}
-                  color='#90dbf4'
+                  color="#90dbf4"
                 />
                 <Series
-                  name='Emotion'
-                  type='areaspline'
+                  name="Emotion"
+                  type="areaspline"
                   data={chartData[0].emotion}
-                  color='#8eecf5'
+                  color="#8eecf5"
                 />
                 <Series
-                  name='IQ'
-                  type='areaspline'
+                  name="IQ"
+                  type="areaspline"
                   data={chartData[0].iq}
-                  color='#98f5e1'
+                  color="#98f5e1"
                 />
               </YAxis>
 
               <Tooltip
-                valueDecimals='0'
+                valueDecimals="0"
                 shared
                 positioner={tooltipPositioner}
-                style={{ fontSize: '14px' }}
+                style={{ fontSize: "14px" }}
                 headerFormat='<span style="color:#888;font-weight:bold">{point.key}</span><br/>'
                 pointFormat='<span style="color: {series.color}">{series.name} </span><span style="color:#bbb;font-weight:bold"><b>{point.y}</b></span><br/>'
               />
             </HighchartsChart>
           </HighchartsProvider>
-          <HighchartsProvider Highcharts={Highcharts} key='Person2'>
+          <HighchartsProvider Highcharts={Highcharts} key="Person2">
             <HighchartsChart plotOptions={plotOptions} chart={chartOptions}>
-              <Title align='left' margin={30} x={30}>
-                {new Date(birthDate2).toLocaleDateString('en-us', {
-                  weekday: 'short',
-                  year: 'numeric',
-                  month: 'short',
-                  day: 'numeric',
+              <Title align="left" margin={30} x={30}>
+                {new Date(birthDate2).toLocaleDateString("en-us", {
+                  weekday: "short",
+                  year: "numeric",
+                  month: "short",
+                  day: "numeric",
                 })}
               </Title>
-              <XAxis crosshair type='datetime' />
+              <XAxis crosshair type="datetime" />
               <YAxis>
                 <Series
-                  name='Physical'
-                  type='areaspline'
+                  name="Physical"
+                  type="areaspline"
                   data={chartData[1].physical}
-                  color='#fde4cf'
+                  color="#fde4cf"
                 />
                 <Series
-                  name='Emotion'
-                  type='areaspline'
+                  name="Emotion"
+                  type="areaspline"
                   data={chartData[1].emotion}
-                  color='#ffcfd2'
+                  color="#ffcfd2"
                 />
                 <Series
-                  name='IQ'
-                  type='areaspline'
+                  name="IQ"
+                  type="areaspline"
                   data={chartData[1].iq}
-                  color='#f1c0e8'
+                  color="#f1c0e8"
                 />
               </YAxis>
 
               <Tooltip
-                valueDecimals='0'
+                valueDecimals="0"
                 shared
                 positioner={tooltipPositioner}
-                style={{ fontSize: '14px' }}
+                style={{ fontSize: "14px" }}
                 headerFormat='<span style="color:#888;font-weight:bold">{point.key}</span><br/>'
                 pointFormat='<span style="color: {series.color}">{series.name} </span><span style="color:#bbb;font-weight:bold"><b>{point.y}</b></span><br/>'
               />
@@ -203,9 +204,9 @@ const Chart = ({ birthDate1, birthDate2, midDate }) => {
         </div>
       </Wrapper>
     )
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
 
-const Wrapper = tw.div``
+const Wrapper = tw.div``;
